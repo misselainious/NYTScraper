27916181a4fde46e6b398b0fa411bf1aa95e640f
Elaine <elaine.cadman@gmail.com>
2019-02-06 13:01:58 -0800

    app.listen changed

--- server.js
+++ server.js
@@ -206,6 +206,9 @@ app.get("/deletenote/:id", function(req, res) {
   });
 });
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT + " you got this...");
+// app.listen(PORT, function() {
+//   console.log("App listening on PORT " + PORT + " you got this...");
+// });
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
